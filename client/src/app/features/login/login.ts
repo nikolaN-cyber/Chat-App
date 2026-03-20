b17129d3b55f7cb0ba6be50bfdad10cc9d03a269
Nikola <nnikola2409@gmail.com>
2026-03-20 15:23:29 +0100

    UI updated

--- client/src/app/features/login/login.ts
+++ client/src/app/features/login/login.ts
@@ -1,10 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { authStore } from '../../shared/store/auth.store';
+import { MatCard } from "@angular/material/card";
+import { MatCardHeader } from '@angular/material/card';
+import { MatCardTitle } from '@angular/material/card';
+import { MatFormField } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatCard, MatCardHeader, MatCardTitle, MatFormField, MatFormFieldModule, MatInputModule, MatIconModule, RouterModule, MatCardModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
